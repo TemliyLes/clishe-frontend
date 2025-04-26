f9448c3301decc4ae487c695f31e5dc3b9d22403
les <seinistdasseinnigcht@gmail.com>
2025-04-26 15:13:04 +0400

    cursor-pointer

--- components/tags/Tag.vue
+++ components/tags/Tag.vue
@@ -1,6 +1,6 @@
 <template>
     <div @click="navigateTo(data.id)"
-        class="text-[16px] inline-block rounded-3xl text-main monster px-4 sm:px-5 whitespace-nowrap py-2 border border-1 border-main">
+        class="text-[16px] cursor-pointer inline-block rounded-3xl text-main monster px-4 sm:px-5 whitespace-nowrap py-2 border border-1 border-main">
         {{ data.name }}
     </div>
 </template>
